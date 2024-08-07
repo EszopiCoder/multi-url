a897f1056405646af162c5851ca3d6f7eba9d832
EszopiCoder <pharm.coder@gmail.com>
2024-08-07 00:00:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let test = [
+  "https://google.com/",
+  "https://www.yahoo.com/"
+];
+let clinical = [
+  "https://outlook.office.com/",
+  "https://apw.swmed.edu/agentportal/#login",
+  "https://resources.therigy.com/tas/1.0.0/saml2/utsouthwestern",
+  "https://online.lexi.com/lco/action/home",
+  "https://www.clinicalkey.com/pharmacology/",
+  "https://www.micromedexsolutions.com/micromedex2/librarian?partner=true>"
+]
+
+function openMultiURL(links) {
+  let urlLog = 'Log:';
+  for (let i = 0; i < links.length; i++) {
+    n = window.open(links[i], '_blank');
+    if (n != null) {
+      urlLog += '<br>Open successfully: <a target="_blank" href="' + links[i] + '">' + links[i] +'</a>';
+    } else {
+      urlLog += '<br>Failed to open: <a target="_blank" href="' + links[i] + '">' + links[i] +'</a>';
+    }
+    document.getElementById('output').innerHTML = urlLog;
+  }
+}
