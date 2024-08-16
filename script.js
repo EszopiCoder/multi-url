b7d77d939ff998fbd462ad77ed45186078eab914
EszopiCoder <pharm.coder@gmail.com>
2024-08-15 21:40:17 -0500

    Update script.js
    Testing new code

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ let clinical = [
 function openMultiURL(links) {
   let urlLog = 'Log:';
   for (let i = 0; i < links.length; i++) {
-    let n = window.open(links[i], '_blank');
+    let n = window.open('', '_blank');
+    n.document.write('Loading...');
+    n.location.href = links[i];
     if (n != null) {
       urlLog += '<br>Open successfully: <a target="_blank" href="' + links[i] + '">' + links[i] +'</a>';
     } else {
